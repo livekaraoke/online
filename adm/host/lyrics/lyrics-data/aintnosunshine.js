e22cd1751053c0671ae5d955a8c770f41c8d6d9f
livekaraoke <livekaraoke@protonmail.com>
2026-06-09 23:02:55 +0200

    Create aintnosunshine.js

---
+++ adm/host/lyrics/lyrics-data/aintnosunshine.js
@@ -0,0 +1,106 @@
+window.songData = {
+  "id": "aintnosunshine",
+  "title": "Ain't no sunshine",
+  "artist": "Bill Withers",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Ain't no sunshine when she's gone",
+        "It's not warm when she's away",
+        "",
+        "Ain't no sunshine when she's gone",
+        "And she's always gone too long",
+        "",
+        "Anytime she goes away"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Wonder this time where she's gone",
+        "Wonder if she's gone to stay",
+        "",
+        "Ain't no sunshine when she's gone",
+        "And this house just ain't no home",
+        "",
+        "Anytime she goes away"
+      ]
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "MIDDLE"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "And I know, I know, I know, I know",
+        "I know, I know, I know, I know, I know",
+        "",
+        "I know, I know, I know, I know, I know",
+        "I know, I know, I know, I know, I know",
+        "",
+        "I know, I know, I know, I know, I know",
+        "I know, I know",
+        "",
+        "Hey, I oughtta leave that young thing alone",
+        "But ain't no sunshine when she's gone"
+      ]
+    },
+    {
+      "type": "note",
+      "text": "(After the stop:)"
+    },
+    {
+      "type": "section",
+      "text": "LAST VERSE"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Ain't no sunshine when she's gone",
+        "Only darkness every day",
+        "",
+        "Ain't no sunshine when she's gone",
+        "And this house just ain't no home",
+        "",
+        "Anytime she goes away"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "ENDING"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Anytime she goes away",
+        "Anytime she goes away",
+        "Anytime she goes away"
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
