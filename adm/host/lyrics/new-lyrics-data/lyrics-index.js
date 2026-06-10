89b9ee5d4e895051654fc64c976b96d340a80305
livekaraoke <livekaraoke@protonmail.com>
2026-06-10 10:06:50 +0200

    Create lyrics-index.js

---
+++ adm/host/lyrics/new-lyrics-data/lyrics-index.js
@@ -0,0 +1,12 @@
+const lyricsIndex = [
+  {
+    title: "Ain't No Sunshine",
+    artist: "Bill Withers",
+    file: "aintnosunshine-billwithers.js"
+  },
+  {
+    title: "All Summer Long",
+    artist: "Kid Rock",
+    file: "all-summer-long.js"
+  }
+];
