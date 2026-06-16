e09fa5d102c361d4f52f96ca7dd50581b0a3cd78
livekaraoke <livekaraoke@protonmail.com>
2026-06-16 06:31:32 +0200

    Add files via upload

---
+++ adm/host/lyrics/new-lyrics-data/myway.js
@@ -0,0 +1,99 @@
+window.song_my_way = {
+  "id": "mywayfranksinatra",
+  "title": "My Way",
+  "artist": "Frank Sinatra",
+  "userBpm": "75",
+  "originalBpm": "76",
+  "capo": "",
+  "key": "D",
+  "sections": [
+    {
+      "type": "lyrics",
+      "title": "INTRO",
+      "html": "Intro :    |   D   |   Dmaj7   |   D   |   Dmaj7   |",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "VERSE 1",
+      "html": "D               Dmaj7                D7            B7<div>And now, the end is near, and so I face the final curtain,</div><div>    Em7                Em7/D             Em7/C#   A7       D</div><div>My friend. I'll say it clear, I'll state my case of which I'm certain.</div><div>     D                   D7                   G             Gm</div><div>I've lived a life that's full, I traveled each and every highway,</div><div>    D                    A       Asus4   G    D</div><div>And more, much more than this, I did it my way.</div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "VERSE 2",
+      "html": "D               Dmaj7             D7                B7<div>Regrets, I've had a few, but then again, too few to mention.</div><div>  Em7               Em7/D            Em7/C#   A7    D</div><div>I did what I had to do, And saw it thru without exemption.</div><div>  D                    D7                              G              Gm</div><div>I planned each charted course, Each careful step along the byway,</div><div>    D                    A     Asus4    G        D</div><div>And more, much more than this, I did it my way.</div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "CHORUS",
+      "html": "<b><font color=\"#ffd400\">               D                   D7                               G                     Gsus4 G</font></b><div><b><font color=\"#ffd400\">Yes there were times, I'm sure you knew, When I bit off more than I could  </font></b></div><div><b><font color=\"#ffd400\">            Em               A7                            F#m               Bm7                   chew,</font></b></div><div><b><font color=\"#ffd400\">But thru it all, when there was doubt, I ate it up, and spit it out.</font></b></div><div><b><font color=\"#ffd400\">           Em               A             Asus4    G       D</font></b></div><div><b><font color=\"#ffd400\">I faced it all, and I stood tall, And did it my way.</font></b></div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "VERSE 3",
+      "html": "D               Dmaj7                            D7                B7<div>I've loved, I've laughed and cried. I've had my fill; my share of losing.</div><div>  Em7               Em7/D                 Em7/C#   A7    D</div><div>And now, as tears subside, I find it all so  amusing.</div><div>  D                    D7                 G              Gm</div><div>To think I did all that; And may I say - not in a shy way,</div><div>    D   A       Asus4        G    D</div><div>\"Oh no, oh no not me, I did it my way\".</div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "CHORUS",
+      "html": "<b><font color=\"#ffd400\">               D               D7                               G                        %</font></b><div><b><font color=\"#ffd400\">For what is a man, what has he got? If not himself, then he has naught.</font></b></div><div><b><font color=\"#ffd400\">         Em7               A7                        F#m                Bm7</font></b></div><div><b><font color=\"#ffd400\">To say the things he truly feels;  And not the words of one who kneels.</font></b></div><div><b><font color=\"#ffd400\">                   Em7                        A         Asus4       G     D</font></b></div><div><b><font color=\"#ffd400\">The record shows I took the blows - And did it my way!</font></b></div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    },
+    {
+      "type": "tab",
+      "title": "OUTRO",
+      "html": "<b>e|-2~----2-3-5-5---4-5-7-5~~----5-5-7-5~----3-3-2--5—3--~~---|</b><div><b>B|-3~--------------------------------------------------------|</b></div><div><b>G|-2~--------------------------------------------------------|</b></div><div><b>D|-0~--------------------------------------------------------|</b></div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Courier New",
+        "color": "white",
+        "isTab": true
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "ENDING",
+      "html": "<b><font color=\"#ffd400\">Asus4               G     D</font></b><div><b><font color=\"#ffd400\">Yes,      it was my way.</font></b></div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white",
+        "isTab": false
+      }
+    }
+  ]
+};
