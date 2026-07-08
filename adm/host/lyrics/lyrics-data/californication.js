0610a5d9058ecb09a2bb998a518a33507ac796c8
livekaraoke <livekaraoke@protonmail.com>
2026-07-08 17:56:53 +0200

    Add files via upload

---
+++ adm/host/lyrics/lyrics-data/californication.js
@@ -0,0 +1,184 @@
+window.songData = {
+  "id": "californication",
+  "title": "Californication",
+  "artist": "Red Hot Chili Peppers",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Psychic spies from China try to steal your mind's elation",
+        "And little girls from Sweden dream of silver-screen quotation",
+        "And if you want these kind of dreams, it's Californication"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Short Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "It's the edge of the world and all of Western civilization",
+        "The sun may rise in the East, at least it settled in a final location",
+        "It's understood that Hollywood sells Californication"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "PRECHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Pay your surgeon very well to break the spell of aging",
+        "Celebrity skin: is this your chin or is that war you're waging?",
+        "",
+        "First born unicorn",
+        "Hardcore soft porn"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Short Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 3"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Marry me, girl, be my fairy to the world, be my very own constellation",
+        "A teenage bride with a baby inside getting high on information",
+        "And buy me a star on the boulevard, it's Californication"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Short Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 4"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Space may be the final frontier but it's made in a Hollywood basement",
+        "And Cobain, can you hear the spheres singing songs off Station to Station?",
+        "And Alderaan's not far away, it's Californication"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "PRECHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Born and raised by those who praise control of population",
+        "Well, everybody's been there, and I don't mean no vacation",
+        "",
+        "First born unicorn",
+        "Hardcore soft porn"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 5"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Destruction leads to a very rough road but it also breeds creation",
+        "And earthquakes are to a girl's guitar, they're just another good vibration",
+        "And tidal waves couldn't save the world from Californication"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "PRECHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Pay your surgeon very well to break the spell of aging",
+        "Sicker than the rest, there is no test, but this is what you're craving",
+        "",
+        "First born unicorn",
+        "Hardcore soft porn"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication",
+        "Dream of Californication"
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
---
+++ adm/host/lyrics/lyrics-data/whiskeyinthejar.js
@@ -0,0 +1,146 @@
+window.songData = {
+  "id": "whiskeyinthejar",
+  "title": "Whiskey in the Jar",
+  "artist": "Thin Lizzy / Metallica",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "As I was goin' over the Cork and Kerry mountains",
+        "I saw Captain Farrell, and his money he was countin'",
+        "I first produced my pistol and then produced my rapier",
+        "I said, \"Stand and deliver, or the devil, he may take you\", yeah"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "I took all of his money, and it was a pretty penny",
+        "I took all of his money, yeah, and I brought it home to Molly",
+        "She swore that she loved me, no, never would she leave me",
+        "But the devil take that woman, yeah, for you know she tricked me easy"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Musha ring um a do um a da",
+        "Whack for my daddy-o",
+        "Whack for my daddy-o",
+        "There's whiskey in the jar-o"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 3"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Bein' drunk and weary, I went to Molly's chamber",
+        "Takin' money with me, but I never knew the danger",
+        "For about six or maybe seven, yeah, in walked Captain Farrell",
+        "I jumped up, fired my pistols, and I shot him with both barrels, yeah"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Musha ring um a do um a da",
+        "Whack for my daddy-o",
+        "Whack for my daddy-o",
+        "There's whiskey in the jar-o",
+        "Yeah-heh, whiskey",
+        "Yo-ho, whiskey",
+        "(Woah-oh, yeah-yeah)",
+        "(Woah-yo)",
+        "(Woah-oh, yeah)"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 4"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Now some men like the fishin', and some men like the fowlin'",
+        "And some men like to hear, to hear the cannonball a-roarin'",
+        "Me? I like sleepin', especially in my Molly's chamber",
+        "But here I am in prison, here I am with a ball and chain, yeah"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Musha ring um a do um a da, yeah, yeah",
+        "Whack for my daddy-o",
+        "Whack for my daddy-o",
+        "There's whiskey in the jar-o, yeah",
+        "Whiskey in the jar-o, yeah"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "ENDING"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Musha ring um a do um a da",
+        "Musha ring um a do um a da",
+        "Hey, musha ring um a do um a da",
+        "Musha ring um a do um a da, yeah"
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
