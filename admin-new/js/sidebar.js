ac27f9569f228c74ed3f1f3c3ecae55525826b3c
livekaraoke <livekaraoke@protonmail.com>
2026-07-08 10:57:49 +0200

    Update sidebar.js

--- admin-new/js/sidebar.js
+++ admin-new/js/sidebar.js
@@ -99,7 +99,7 @@ function listenSidebarSongRequests() {
           if (!songId) return;
 
           window.location.href =
-            `${location.pathname.includes("/host/") ? "lyricview.html" : "host/lyricview.html"}?id=${encodeURIComponent(songId)}&requestId=${encodeURIComponent(req.id)}`;
+            `../../adm/host/lyricview.html?id=${encodeURIComponent(songId)}&requestId=${encodeURIComponent(req.id)}`;
         };
 
         box.appendChild(btn);
