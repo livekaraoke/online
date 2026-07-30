12d0e93fa76e1c0549add6ce4cfa895750e42886
livekaraoke <livekaraoke@protonmail.com>
2026-07-30 03:29:55 +0200

    Add files via upload

---
+++ adm/host/lyrics/lyrics-data/clocks.js
@@ -0,0 +1,146 @@
+window.songData = {
+  "id": "clocks",
+  "title": "Clocks",
+  "artist": "Coldplay",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Lights go out and I can't be saved",
+        "Tides that I tried to swim against",
+        "Brought me down upon my knees",
+        "Oh I beg, I beg and plead, singing",
+        "",
+        "Come out of things unsaid",
+        "Shoot an apple off my head, and a",
+        "Trouble that can't be named",
+        "Tiger's waiting to be tamed, singing"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "You... are...",
+        "You... are..."
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Short Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Confusion never stops",
+        "Closing walls and ticking clocks",
+        "Gonna come back and take you home",
+        "I could not stop the tune now known, singing",
+        "",
+        "Come out upon my seas",
+        "Cursed missed opportunities",
+        "Am I a part of the cure?",
+        "Or am I part of the disease? Singing"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "You... are...",
+        "You... are...",
+        "",
+        "You... are...",
+        "You... are..."
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Short Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "You... are...",
+        "You... are..."
+      ]
+    },
+    {
+      "type": "section",
+      "text": "BRIDGE"
+    },
+    {
+      "type": "bridge",
+      "lines": [
+        "And nothing else compares",
+        "Oh nothing else compares",
+        "And nothing else compares"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "You... are...",
+        "You... are..."
+      ]
+    },
+    {
+      "type": "section",
+      "text": "ENDING"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Home, home where I wanted to go",
+        "Home, home where I wanted to go",
+        "",
+        "Home, home where I wanted to go",
+        "Home, home where I wanted to go,",
+        "Home."
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
