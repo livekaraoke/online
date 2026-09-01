ae806ea40114d8f2b3bcc3156ed6b4ec9316dd9f
livekaraoke <livekaraoke@protonmail.com>
2026-09-01 22:41:10 +0200

    Add files via upload

---
+++ adm/host/lyrics/lyrics-data/idontwanttotalkaboutit.js
@@ -0,0 +1,104 @@
+window.songData = {
+  "id": "idontwanttotalkaboutit",
+  "title": "I Don't Want to Talk About It",
+  "artist": "Rod Stewart",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "I can tell by your eyes",
+        "That you've probably been crying forever",
+        "",
+        "And the stars in the sky don't mean nothing to you",
+        "They're a mirror"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "I don't wanna talk about it",
+        "How you broke my heart",
+        "",
+        "If I stay here just a little bit longer",
+        "If I stay here, won't you listen",
+        "",
+        "To my heart?",
+        "Oh, my heart"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "If I stand all alone",
+        "Will the shadow hide the color of my heart?",
+        "",
+        "Blue for the tears, black for the night's fears",
+        "The stars in the sky don't mean nothing to you",
+        "",
+        "They're a mirror"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "I don't wanna talk about it",
+        "How you broke my heart",
+        "",
+        "If I stay here just a little bit longer",
+        "If I stay here, won't you listen",
+        "",
+        "To my heart?",
+        "Oh, my heart"
+      ]
+    },
+    {
+      "type": "section-seperator"
+    },
+    {
+      "type": "note",
+      "text": "Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "I don't wanna talk about it",
+        "How you broke this ole heart",
+        "",
+        "If I stay here just a little bit longer",
+        "If I stay here, won't you listen to my heart?",
+        "",
+        "Oh, my heart",
+        "My heart",
+        "Oh, my heart"
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
