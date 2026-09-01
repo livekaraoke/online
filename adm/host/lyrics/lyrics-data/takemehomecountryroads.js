3ebb6c12f4aba229c5e532011274e22f06713887
livekaraoke <livekaraoke@protonmail.com>
2026-09-01 22:25:29 +0200

    Add files via upload

---
+++ adm/host/lyrics/lyrics-data/takemehomecountryroads.js
@@ -0,0 +1,124 @@
+window.songData = {
+  "id": "takemehomecountryroads",
+  "title": "Take Me Home, Country Roads",
+  "artist": "John Denver",
+  "lyrics": [
+    {
+      "type": "note",
+      "text": "INTRO Instrumental (wait for signal)"
+    },
+    {
+      "type": "section",
+      "text": "VERSE 1"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "Almost Heaven, West Virginia",
+        "Blue Ridge Mountains, Shenandoah River",
+        "",
+        "Life is old there, older than the trees",
+        "Younger than the mountains, blowing like a breeze"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Country roads, take me home",
+        "To the place I belong",
+        "",
+        "West Virginia, mountain momma",
+        "Take me home, country roads"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "VERSE 2"
+    },
+    {
+      "type": "verse",
+      "lines": [
+        "All my memories gather 'round her",
+        "Miner's lady, stranger to blue water",
+        "",
+        "Dark and dusty, painted on the sky",
+        "Misty taste of moonshine, teardrop in my eye"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Country roads, take me home",
+        "To the place I belong",
+        "",
+        "West Virginia, mountain momma",
+        "Take me home, country roads"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "BRIDGE"
+    },
+    {
+      "type": "bridge",
+      "lines": [
+        "I hear her voice in the morning hour, she calls me",
+        "The radio reminds me of my home far away",
+        "",
+        "Driving down the road, I get a feeling",
+        "That I should have been home yesterday, yesterday"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS"
+    },
+    {
+      "type": "chorus",
+      "lines": [
+        "Country roads, take me home",
+        "To the place I belong",
+        "",
+        "West Virginia, mountain momma",
+        "Take me home, country roads"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "CHORUS REPEAT"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Country roads, take me home",
+        "To the place I belong",
+        "",
+        "West Virginia, mountain momma",
+        "Take me home, country roads"
+      ]
+    },
+    {
+      "type": "section",
+      "text": "ENDING"
+    },
+    {
+      "type": "chorusrepeat",
+      "lines": [
+        "Take me home, (Down) country roads",
+        "Take me home, (Down) country roads"
+      ]
+    },
+    {
+      "type": "action",
+      "text": "[ END ]"
+    }
+  ]
+};
