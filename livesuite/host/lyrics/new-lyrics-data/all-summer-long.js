12b9e9e91a674989526bd7a57278d99d89eae4eb
livekaraoke <livekaraoke@protonmail.com>
2026-09-02 02:53:16 +0200

    Add files via upload

---
+++ livesuite/host/lyrics/new-lyrics-data/all-summer-long.js
@@ -0,0 +1,44 @@
+window.song_all_summer_long = {
+  "id": "allsummerlongkidrock",
+  "title": "All Summer Long",
+  "artist": "Kid Rock",
+  "userBpm": "101",
+  "originalBpm": "101?",
+  "capo": "0",
+  "key": "D",
+  "sections": [
+    {
+      "type": "lyrics",
+      "title": "VERSE",
+      "html": "D  C                   G<div>It was <font color=\"#7cff3a\">19</font>89, my thoughts were <font color=\"#7cff3a\">short </font>my hair was long</div><div><font color=\"#7cff3a\">Caught </font>somewhere bet<u>ween</u> a boy and <font color=\"#7cff3a\">man</font></div><div><font color=\"#7cff3a\">She </font>was seven<u>teen</u> and she was <font color=\"#7cff3a\">far </font>from in between</div><div>It was <font color=\"#7cff3a\">summer</font>time in <u>Northern</u> <font color=\"#7cff3a\">Michigan</font></div>",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white"
+      }
+    },
+    {
+      "type": "separator"
+    },
+    {
+      "type": "tab",
+      "title": "SOLO",
+      "html": "<b>    D\n<!--StartFragment-->e|-------2--------------|<br>B|-----3---3------------|<br>G|---2-------2----------|<br>D|--0-------------------|<br>A|----------------------|</b><!--EndFragment-->\n\n<!--EndFragment-->",
+      "collapsed": true,
+      "style": {
+        "fontFamily": "Courier New",
+        "color": "white"
+      }
+    },
+    {
+      "type": "lyrics",
+      "title": "VERSE 2",
+      "html": "wewagregregreg",
+      "collapsed": false,
+      "style": {
+        "fontFamily": "Verdana",
+        "color": "white"
+      }
+    }
+  ]
+};
---
+++ livesuite/host/lyrics/new-lyrics-data/lyrics-index.js
@@ -0,0 +1,17 @@
+const lyricsIndex = [
+  {
+    title: "Ain't No Sunshine",
+    artist: "Bill Withers",
+    file: "aintnosunshine-billwithers.js"
+  },
+  {
+    title: "All Summer Long",
+    artist: "Kid Rock",
+    file: "all-summer-long.js"
+  },
+  {
+    title: "My Way",
+    artist: "Frank Sinatra",
+    file: "my-way.js"
+  }
+];
