fedcc040cf8cc15c13a73412fabb118471ababb1
livekaraoke <livekaraoke@protonmail.com>
2026-09-09 17:42:52 +0200

    Update currentdb.js

--- billylee26/db/currentdb.js
+++ billylee26/db/currentdb.js
@@ -1,12 +1,13 @@
 (function () {
   "use strict";
   window.SITE_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC4gSodXM35E2ZdYaz6mrCvTUYzW75ZCBk",
-    authDomain: "livekaraokemt.firebaseapp.com",
-    projectId: "livekaraokemt",
-    storageBucket: "livekaraokemt.firebasestorage.app",
-    messagingSenderId: "425980659562",
-    appId: "1:425980659562:web:892ddcd53fb209d1114713"
+    apiKey: "AIzaSyAkJ6yKFE8jgcDoWtZfQKmHjhBk4rfZ8Fg",
+    authDomain: "livekaraokesuite.firebaseapp.com",
+    projectId: "livekaraokesuite",
+    storageBucket: "livekaraokesuite.firebasestorage.app",
+    messagingSenderId: "25324781952",
+    appId: "1:25324781952:web:ca9467eecce90574ee8165",
+    measurementId: "G-J1DVP1T0HW"
   };
-  window.SITE_FIREBASE_PROJECT = "LIVEKARAOKEMT";
+  window.SITE_FIREBASE_PROJECT = "LIVEKARAOKESUITE";
 })();
